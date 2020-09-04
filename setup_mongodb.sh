978db9793bb03bba118ace20237f01db835f6992
Richard Li <licai@cs.utah.edu>
2020-09-03 18:45:47 -0700

    use -j `nproc` to make the cmake faster

--- setup_mongodb.sh
+++ setup_mongodb.sh
@@ -3,17 +3,17 @@ tar xzf mongo-c-driver-1.17.0.tar.gz
 cd mongo-c-driver-1.17.0
 mkdir cmake-build
 cd cmake-build
-cmake -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
-cmake --build .
+cmake -j `nproc` -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF ..
+cmake -j `nproc` --build .
 sudo cmake --build . --target install
 cd ../..
 curl -OL https://github.com/mongodb/mongo-cxx-driver/releases/download/r3.6.0/mongo-cxx-driver-r3.6.0.tar.gz
 tar -xzf mongo-cxx-driver-r3.6.0.tar.gz
 cd mongo-cxx-driver-r3.6.0/build
-cmake ..                                \
+cmake -j `nproc` ..                                \
     -DCMAKE_BUILD_TYPE=Release          \
     -DCMAKE_INSTALL_PREFIX=/usr/local
-cmake --build .
+cmake -j `nproc` --build .
 sudo cmake --build . --target install
 cd ../..
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
