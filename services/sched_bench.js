eea31006ea1114ef36bf54015202835aacd114da
Tian Zhang <tian@localhost>
2021-06-30 20:05:04 -0600

    Add scheduler benchmark.

---
+++ services/sched_bench.js
@@ -0,0 +1,36 @@
+function async_call(req_id, call_id, service, func, args) {
+    return new Promise(function(resolve, reject) {
+        Call(req_id, call_id, service, func,
+            function(error, result) {
+                if (error) {
+                    return reject(error);
+                }
+                return resolve(result);
+            },
+            args);
+    });
+}
+
+function run_10ms(req_id, call_id, args) {
+    let endtime = new Date().getTime() + 10;
+    while (new Date().getTime() < endtime) {
+    }
+
+    let rep = new Object();
+    rep._status = 200;
+    rep._message = "OK";
+    let ret = JSON.stringify(rep);
+    Reply(req_id, call_id, ServiceName, ret);}
+}
+
+function run_100ms(req_id, call_id, args) {
+    let endtime = new Date().getTime() + 100;
+    while (new Date().getTime() < endtime) {
+    }
+
+    let rep = new Object();
+    rep._status = 200;
+    rep._message = "OK";
+    let ret = JSON.stringify(rep);
+    Reply(req_id, call_id, ServiceName, ret);}
+}
