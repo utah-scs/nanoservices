b78951ba44df5174c12fd2b483b3c154b7941a34
Tian Zhang <candy@node-0.candy-256333.sandstorm.emulab.net>
2025-05-04 03:44:22 -0600

    Modify setup_v8.sh to install latest ninja.

--- setup_v8.sh
+++ setup_v8.sh
@@ -7,11 +7,9 @@ sudo apt-get update
 ./build/install-build-deps.sh
 sudo apt-get install -y libhiredis-dev
 sudo apt-get install -y python
-wget https://github.com/ninja-build/ninja/releases/download/v1.9.0/ninja-linux.zip
-unzip ninja-linux.zip
-sudo cp ninja /usr/local/bin/
 sudo apt-get install -y clang
 sudo apt install generate-ninja
+sudo apt install ninja-build
 gn gen out/x64.release --args='dcheck_always_on = false is_clang = false is_component_build = false is_debug = false target_cpu = "x64" use_custom_libcxx = false v8_monolithic = true v8_use_external_startup_data = false v8_enable_pointer_compression = false'
 ninja -C out/x64.release v8_monolith
 cd ../
