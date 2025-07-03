975665ae9663f1a7e3b7fca762d25478b28314f1
Tian Zhang <candy@node-1.candy-260893.sandstorm.emulab.net>
2025-07-03 02:56:49 -0600

    Update setup_lua.sh

--- setup_lua.sh
+++ setup_lua.sh
@@ -2,9 +2,9 @@ git clone https://luajit.org/git/luajit.git
 cd luajit
 make && sudo make install
 cd ..
-wget https://luarocks.org/releases/luarocks-3.11.1.tar.gz
-tar zxpf luarocks-3.11.1.tar.gz
-cd luarocks-3.11.1
+wget https://luarocks.org/releases/luarocks-3.12.2.tar.gz
+tar zxpf luarocks-3.12.2.tar.gz
+cd luarocks-3.12.2
 ./configure && make && sudo make install
 sudo luarocks install luasocket
 cd ..
